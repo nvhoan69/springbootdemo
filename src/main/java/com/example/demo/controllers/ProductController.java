dcbd9fadb170b873484e9c8114a38e14a28dd700
Thien Khu Tu <hoannv41@gmail.com>
2022-07-11 15:04:16 +0700

    that's it

---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Product;
+import com.example.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/Products")
+public class ProductController {
+    // DI = Dependency Injection
+    @Autowired
+    private ProductRepository repository; // auto create
+
+    @GetMapping("")
+    // this request is: http://localhost:8080/api/v1/Products
+    List<Product> getAllProducts() {
+        return  repository.findAll();
+    }
+}
